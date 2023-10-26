a2516a2fde095fe1b8d07681e26d1c181618b7bc
dcorread <d.corread@uniandes.edu.co>
2023-10-25 21:18:04 -0400

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MISW-4203-DesarrolloMovil-Frontend"
+include(":app")
+ 
