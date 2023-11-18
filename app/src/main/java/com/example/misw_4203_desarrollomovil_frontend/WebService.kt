6bf4f903ba3efad9068ed2a618e1004aa64325ff
lsochoa2023 <lucystell@192.168.1.15>
2023-11-18 17:03:27 -0500

    HU listar Album, HU Get Album y Formulario Crear Album

--- app/src/main/java/com/example/misw_4203_desarrollomovil_frontend/WebService.kt
+++ app/src/main/java/com/example/misw_4203_desarrollomovil_frontend/WebService.kt
@@ -1,7 +1,9 @@
 package com.example.misw_4203_desarrollomovil_frontend
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.POST
 
 interface WebService {
     @GET("/musicians")
@@ -11,4 +13,18 @@ interface WebService {
     suspend fun getMusiciansbyId(
         @Path("id") musicianId: String
     ): Response<Musicians>
+
+    @GET("/albums")
+    suspend fun getAlbumes(): Response<List<AlbumList>>
+
+    @GET("/albums/{id}")
+    suspend fun getAlbumbyId(
+        @Path("id") albumId: String
+    ): Response<AlbumList>
+
+    @POST("/albums")
+    suspend fun addAlbum(
+        @Body album: AlbumDto
+    ): Response<AlbumDtoResponse>
+
 }
--- app/src/main/java/com/example/misw_4203_desarrollomovil_frontend/screens/ListadoArtistas.kt
+++ app/src/main/java/com/example/misw_4203_desarrollomovil_frontend/screens/ListadoArtistas.kt
@@ -1,12 +1,10 @@
 package com.example.misw_4203_desarrollomovil_frontend.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -24,10 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.misw_4203_desarrollomovil_frontend.Musicians
 import com.example.misw_4203_desarrollomovil_frontend.MusiciansViewModel
